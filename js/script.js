7b218812c11a6d367c3f0959f5b3d9ca5fdd92f5
Hootman77 <132826470+Hootman77@users.noreply.github.com>
2023-05-24 07:23:09 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,103 +1,78 @@
 
-SAMPLE_CARDS = [{
-    movie_name : "Jumanji: Welcome to the Jungle",
-    image : "text",
-    info : "This movie is about a board game that comes to life",
-    genre : "Action", 
-    genre2 :  "",
-    year: "2017"
-}, 
-{   movie_name : "Hitman & Bodyguard 2",
-    image : "Text",
-    info : "This movie is about hitmen protecting an important client",
-    genre : "Action",
-    genre2 :  "Comedy",
-    year: "2021"
-},
-{   movie_name : "Shang-Chi",
-    image : "Text",
-    info : "This movie is about a superhero discovering his power",
-    genre : "Action", 
-    genre2 : "Sci-Fi",
-    year: "2021"
-},
-{   movie_name : "Eternals",
-    image : "Text",
-    info : "This movie is about ancient guardians of the Earth",
-    genre : "Action", 
-    genre2 : "Sci-Fi",
-    year: "2021"
-},
-{   movie_name : "Spectre",
-    image : "Text",
-    info : "This movie is about James Bond infiltrating a secret society",
-    genre : "Action",
-    genre2 :  "",
-    year: "2015"
-},
-{   movie_name : "Money Heist",
-    image : "Text",
-    info : "This movie is about pulling off the greatest ever heist",
-    genre : "Action",
-    genre2 : "Sci-Fi",
-    year: "2017"
-},
-{   movie_name : "The Wolverine",
-    image : "Text",
-    info : "This movie is about the origins of Logan the Wolverine",
-    genre : "Action",
-    genre2 : "Sci-Fi",
-    year: "2013"
-},
-{   movie_name : "Johnny English",
-    image : "Text",
-    info : "This movie is about Johnny English the sophisitcated and suave English spy",
-    genre : "Action",
-    genre2 : "Comedy",
-    year: "2018"
-}
-
-]
-
+let SAMPLE_CARDS = []
+let ids = [];
    
 const searchInput = document.querySelector("[data-search]")
 const dataMovieTemplate = document.querySelector("[data-movie-template]")
 const movieCardContainer = document.querySelector("[data-movie-card-container]")
 
-let test = []
+
+const msg = "1"
+
+function Toggle2() {
+    
+
+    document.getElementById("body6").innerHTML = "Result not found within the database"
+    
+
+}
+
+function timeout(){
+    
+    setTimeout(Toggle2, 2000);
+}
+
+function same(){
+    var x = document.getElementById("cards");
+    if(window.getComputedStyle(x).display === "none" ){
+        timeout();
+        
+    }
+    else {
+        console.log("error");
+    } 
+}
 
 searchInput.addEventListener("input", (e) => {
+    const test = ids.toString()
     const value = e.target.value.toLowerCase()
-    test.forEach(test => {
-        const isVisible = test.movie_name.toLowerCase().includes(value) || test.genre.toLowerCase().includes(value) || test.genre2.toLowerCase().includes(value) || test.year.toLowerCase().includes(value)  
-        test.element.classList.toggle("hide", !isVisible)
+    ids.forEach(user => {
+        const isVisible = user.title.toString().toLowerCase().includes(value) || user.genre.toString().toLowerCase().includes(value) || user.year.toString().toLowerCase().includes(value) 
+        user.element.classList.toggle("hide", !isVisible);
     })
+    same();
     
-    console.log(test)
 })
 
-const data = SAMPLE_CARDS
 
-const Display = (SAMPLE_CARDS => {
-    test = data.map(SAMPLE_CARDS => {
-    const card = dataMovieTemplate.content.cloneNode(true).children[0]
-    const header = card.querySelector("[data-header]")
-    const body1 = card.querySelector("[data-body1]")
-    const body2 = card.querySelector("[data-body2]")
-    const body3 = card.querySelector("[data-body3]") 
-    const body4 = card.querySelector("[data-body4]")
-    const body5 = card.querySelector("[data-body5]")
-    header.textContent = SAMPLE_CARDS.movie_name
-    body1.textContent = SAMPLE_CARDS.image
-    body2.textContent = SAMPLE_CARDS.info
-    body3.textContent = SAMPLE_CARDS.genre
-    body4.textContent = SAMPLE_CARDS.genre2
-    body5.textContent = SAMPLE_CARDS.year
 
-    movieCardContainer.append(card)
-    console.log(SAMPLE_CARDS)
-    return { movie_name: SAMPLE_CARDS.movie_name, genre: SAMPLE_CARDS.genre, genre2: SAMPLE_CARDS.genre2, year: SAMPLE_CARDS.year, element: card}
-    });
-})
+fetch("https://imdb-top-100-movies.p.rapidapi.com/", {
+    method: 'GET',
+	headers: {
+	'X-RapidAPI-Key': '4af2ca0c30msh71b080dcf82b59dp1ff7d6jsn34e7fa638cae',
+	'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+    }})
+    .then(res => res.json())
+    .then(data => {
+        ids = data.map(id => {
+           const card = dataMovieTemplate.content.cloneNode(true).children[0]
+           const header = card.querySelector("[data-header]")
+           const body1 = card.querySelector("[data-body1]")
+           const body2 = card.querySelector("[data-body2]")
+           const body3 = card.querySelector("[data-body3]") 
+           const body5 = card.querySelector("[data-body5]")
+           header.textContent = id.title
+           body1.src = id.image
+           body3.textContent = id.genre
+           body5.textContent = id.year
+           movieCardContainer.append(card)
+           return { title: id.title, genre: id.genre, year: id.year, element: card}
+        })
+    })
 
-Display();
+    
+
+
+
+
+
